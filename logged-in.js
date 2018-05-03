a10604615dabc9dbd391bb5266b96d04445833b8
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2018-05-03 13:12:48 -0500

    added redirect window

--- logged-in.js
+++ logged-in.js
@@ -1,16 +1,48 @@
-let url = window.location.href;
-let access_token = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
-console.log(access_token);
+
+function getToken() {
+	let url = window.location.href;
+	let access_token = url.split('=');
+	let token_type = access_token[1].split('&');
+	let token = token_type[0];
+	console.log(token);
+}
 
 //listen for submit
 function listenForSubmit() {
+	$('#container').submit('#search-btn', function(e) {
+		e.preventDefault();
+		let artist = $('#search-box').val();
+		$('#search-box').val('');
+		artistSearchAPI(artist,token);
 
+
+	});
 	//take value from form
 }
 //send to spotify
-function artistSearchAPI(artist) {
-
+function artistSearchAPI(artist,token) {
+	const settings = {
+		url: 'https://api.spotify.com/v1/search',
+		header: {
+			Accept: "application/json",
+			"Content-type" : "application/json",
+			Authorization: `Bearer ${token}`
+		},
+		data: {
+			q: artist,
+			type: 'artist',
+			limit: 20
+		},
+		success: function(response) {
+			console.log(response);
+		},
+		error: function() {
+			alert.error();
+		}
+	}
+	$.ajax(settings);
 }
+
 //render results
 function renderResults() {
 
@@ -60,8 +92,8 @@ function highlightRestart() {
 }
 //run callbacks
 function loadCallbacks() {
-	displayCorrectForm();
 	listenForSubmit();
-}
+	getToken();
+};
 
 $(loadCallbacks);
