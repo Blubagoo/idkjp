c059d6d61c4332d9944bc6363a900f5f7b2459b9
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2018-05-04 11:59:32 -0500

    added redirect window

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function listenForLogin() {
 	$('#container').submit('#login-button', function(e) {
 		e.preventDefault();
 		console.log("button squelched");
-		window.location.href = "https://accounts.spotify.com/authorize?client_id=4f0988fdbe304564abc71c130d26702f&redirect_uri=https://discover-artists.herokuapp.com/logged-in.html&scope=user-read-private%20user-read-email&response_type=token"
+		window.location.href = "https://accounts.spotify.com/authorize?client_id=4f0988fdbe304564abc71c130d26702f&redirect_uri=https://blubagoo.github.io/listen-and-watch/logged-in.html&scope=user-read-private%20user-read-email&response_type=token"
 
 	});
 }
